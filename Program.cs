c167b75249f6805396b5234702cb502f7231ce32
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-25 17:09:57 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿class MyClass
+{
+    static int progress = 0;
+    static CancellationTokenSource _tokenSource = null;
+    static bool pauseFlag=false;
+    public  static void Main(string[] args)
+    {
+        Task.WhenAll(Test());
+        Console.ReadLine();
+
+    }
+
+    private static async Task Test()
+    {
+        _tokenSource = new CancellationTokenSource();
+        var token = _tokenSource.Token;
+        await Task.Run(() => LoopThroughNumbers(100, progress, token));
+        await Task.Run(() => CancelMethod());
+        Console.Write("CountDown Completed.");
+        
+    }
+
+    private static void CancelMethod()
+    {
+        while (true)
+        {
+            if (Console.KeyAvailable)
+            {
+                pauseFlag= true;
+                string action = Console.ReadLine();
+                if (action.ToLower() == "cancel")
+                {
+                    Console.WriteLine("CountDown cancelled.");
+                    _tokenSource.Cancel();
+                    break;
+                }
+               
+            }
+        }
+    }
+
+    private static void LoopThroughNumbers(int count,int progress,CancellationToken token)
+    {
+       for(int i=0;i<count;i++)
+        {
+            if(!pauseFlag)
+            {
+                    Thread.Sleep(1000);
+                    progress=(i*100)/count;
+                    Console.WriteLine("Progress: "+progress);
+                    if(token.IsCancellationRequested)
+                    {
+                        token.ThrowIfCancellationRequested();
+                    }
+            }
+            
+        }
+    }
+} 
